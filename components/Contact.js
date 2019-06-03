24bfd2e8fbced3fed54f23dd80ee0e4c2b2d3c69
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-06-03 23:18:17 +0200

    first commit

---
+++ components/Contact.js
@@ -0,0 +1,34 @@
+var Contact = React.createClass({
+    propTypes: {
+      item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+      return (
+        
+        React.createElement('div', {className: 'contactItem'},
+          React.createElement('img', {
+            className: 'contactImage',
+            src: 'https://image.flaticon.com/icons/svg/236/236831.svg'
+          }),
+          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+            this.props.item.email
+          )
+        )
+          
+        /*
+        <div className={'contactItem'}>
+            <img className={'contactImage'} src={'https://image.flaticon.com/icons/svg/236/236831.svg'}/>
+            <p className={'contactLabel'}>
+                Imię: {this.props.contact.firstName}
+            </p>
+            <a href={'mailto:' + this.props.item.email}>
+                {this.props.item.email}
+            </a>
+        </div>
+        */       
+      )
+    },
+  });
